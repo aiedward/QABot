483450acceade79592e5aaa32c599869b952a284
Alex Tremaskin <tremaskin.alexiy@gmail.com>
2014-06-29 13:06:31 +0400

    Add exception safety to protocol

--- scripts/protocol/protocol.py
+++ scripts/protocol/protocol.py
@@ -1,12 +1,13 @@
-from command import Command, CommandExecutor
 from abc import ABCMeta, abstractmethod
 
 __author__ = 'Alexiy'
 
 
 class Protocol:
-
+    """base protocol class"""
     __metaclass__ = ABCMeta
+
+    FAIL = 'Failed'
 
     @abstractmethod
     def execute(self, command):
@@ -14,11 +15,16 @@ class Protocol:
 
 
 class LocalProtocol(Protocol):
+    """simple protocol for using bots within app"""
 
     def __init__(self, command_executor):
         self._command_executor = command_executor
 
     def execute(self, command):
-        return self._command_executor.execute(command)
+        try:
+            result = self._command_executor.execute(command)
+        except:
+            result = Protocol.FAIL
+        return result
 
 Protocol.register(LocalProtocol)
--- tests/Protocol/local_protocol_test.py
+++ tests/Protocol/local_protocol_test.py
@@ -1,13 +1,23 @@
 import unittest
 from tests.utils.mock import mock_method
-from scripts.protocol.protocol import LocalProtocol
-from scripts.protocol.command import CommandExecutor
+from scripts.protocol.protocol import LocalProtocol, Protocol
+from scripts.protocol.command import CommandExecutor, Command
 
 __author__ = 'Alexiy'
 
 
 def create_local_protocol(command_executor):
     return LocalProtocol(command_executor)
+
+
+class FakeExecutor():
+    """executor for testing"""
+
+    FAIL_COMMAND_NAME = 'fail'
+
+    def execute(self, command):
+        if command.name == FakeExecutor.FAIL_COMMAND_NAME:
+            raise Exception()
 
 
 class LocalProtocolTest(unittest.TestCase):
@@ -21,6 +31,14 @@ class LocalProtocolTest(unittest.TestCase):
         protocol.execute(command)
 
         self.assertEqual(calls, [[command]])
+
+    def test_execute_should_not_throw_error_if_command_do(self):
+        command_executor = FakeExecutor()
+        protocol = create_local_protocol(command_executor)
+
+        result = protocol.execute(Command(FakeExecutor.FAIL_COMMAND_NAME, None))
+
+        self.assertEqual(result, Protocol.FAIL)
 
 if __name__ == "__main__":
     unittest.main()
